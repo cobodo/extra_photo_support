ae4c5a34d037393439aa9fad82f46147408e4b66
cobodo <cobodo@gmail.com>
2018-06-11 22:30:41 +0900

    OpenGraphProtocolにより画像を取得する部分をくくりだした

--- photo_support_extra.rb
+++ photo_support_extra.rb
@@ -1,5 +1,6 @@
 Plugin.create(:photo_support_extra) do
-  defimageopener('marshmallow-qa', %r<\Ahttps?://marshmallow-qa\.com/messages/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}>) do |display_url|
+  # OpenGraphProtocolにより画像を取得する汎用Proc
+  og_getter = Proc.new do |display_url|
     connection = HTTPClient.new
     page = connection.get_content(display_url)
     next nil if page.empty?
@@ -7,4 +8,7 @@ Plugin.create(:photo_support_extra) do
     result = doc.css('meta[property="og:image"]').first
     open(result.attribute('content').to_s)
   end
+
+  # マシュマロ
+  defimageopener('marshmallow-qa', %r<\Ahttps?://marshmallow-qa\.com/messages/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}>, &og_getter)
 end
