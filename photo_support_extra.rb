73daee247c2e474e058ad2287f94676eebdf9c20
cobodo <cobodo@gmail.com>
2018-07-23 18:03:16 +0900

    デバッグ用出力を削除

--- photo_support_extra.rb
+++ photo_support_extra.rb
@@ -7,8 +7,6 @@ Plugin.create(:photo_support_extra) do
     url = Plugin::PhotoSupportExtra::GITHUB_IMAGE_PATTERN.match(display_url) do |m|
       "https://raw.githubusercontent.com/#{m[1]}/#{m[2]}"
     end
-    pp url
-    $stdout.flush
     open(url) if url
   end
 end
