a327585f3cbc3330dc2e34c11c21c6eb4d15824c
cobodo <cobodo@gmail.com>
2018-07-23 17:50:04 +0900

    github.comのリポジトリ側から見たURLへの対応

--- photo_support_extra.rb
+++ photo_support_extra.rb
@@ -1,2 +1,14 @@
+module Plugin::PhotoSupportExtra
+  GITHUB_IMAGE_PATTERN = %r<\Ahttps://github\.com/(\w+/\w+)/blob/(.*\.(#{GdkPixbuf::Pixbuf.formats.flat_map do |f| f.extensions end.join '|'}))\z>
+end
+
 Plugin.create(:photo_support_extra) do
+  defimageopener('github', Plugin::PhotoSupportExtra::GITHUB_IMAGE_PATTERN) do |display_url|
+    url = Plugin::PhotoSupportExtra::GITHUB_IMAGE_PATTERN.match(display_url) do |m|
+      "https://raw.githubusercontent.com/#{m[1]}/#{m[2]}"
+    end
+    pp url
+    $stdout.flush
+    open(url) if url
+  end
 end
